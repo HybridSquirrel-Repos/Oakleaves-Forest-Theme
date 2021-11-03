35dd83fc5e15cd3ef2e9a1537ae0b95e8f392e04
HybridSquirrel <robert.squirrel@mailfence.com>
2021-11-03 13:26:55 +0100

    renamed lualine

--- lua/lualine/themes/forest.lua
+++ lua/lualine/themes/forest.lua
@@ -1,37 +1,37 @@
 local colors = require('forest.colors')
 
-local nord = {}
+local forest = {}
 
 forest.normal = {
-	a = {fg = colors.nord0_gui, bg = colors.nord4_gui, gui = 'bold'},
-	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
-	c = {fg = colors.nord4_gui, bg = colors.nord1_gui},
+	a = {fg = colors.forest0_gui, bg = colors.forest4_gui, gui = 'bold'},
+	b = {fg = colors.forest4_gui, bg = colors.forest10_gui},
+	c = {fg = colors.forest4_gui, bg = colors.forest1_gui},
 }
 
 forest.insert = {
-	a = {fg = colors.nord0_gui, bg = colors.nord14_gui, gui = 'bold'},
-	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
+	a = {fg = colors.forest0_gui, bg = colors.forest14_gui, gui = 'bold'},
+	b = {fg = colors.forest4_gui, bg = colors.forest10_gui},
 }
 
 forest.visual = {
-	a = {fg = colors.nord0_gui, bg = colors.nord9_gui, gui = 'bold'},
-	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
+	a = {fg = colors.forest0_gui, bg = colors.forest9_gui, gui = 'bold'},
+	b = {fg = colors.forest4_gui, bg = colors.forest10_gui},
 }
 
 forest.replace = {
-	a = {fg = colors.nord0_gui, bg = colors.nord11_gui, gui = 'bold'},
-	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
+	a = {fg = colors.forest0_gui, bg = colors.forest11_gui, gui = 'bold'},
+	b = {fg = colors.forest4_gui, bg = colors.forest10_gui},
 }
 
 forest.command = {
-	a = {fg = colors.nord0_gui, bg = colors.nord15_gui, gui = 'bold'},
-	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
+	a = {fg = colors.forest0_gui, bg = colors.forest15_gui, gui = 'bold'},
+	b = {fg = colors.forest4_gui, bg = colors.forest10_gui},
 }
 
-forest.innord10_gui = {
-  a = {fg = colors.nord1_gui, bg = colors.nord0_gui, gui = 'bold'},
-  b = {fg = colors.nord1_gui, bg = colors.nord0_gui},
-  c = {fg = colors.nord1_gui, bg = colors.nord1_gui}
+forest.inforest10_gui = {
+  a = {fg = colors.forest1_gui, bg = colors.forest0_gui, gui = 'bold'},
+  b = {fg = colors.forest1_gui, bg = colors.forest0_gui},
+  c = {fg = colors.forest1_gui, bg = colors.forest1_gui}
 }
 
 return forest
