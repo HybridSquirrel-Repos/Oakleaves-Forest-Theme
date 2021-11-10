46b8becc5c5cd29d7dd6c4f515ebd62387b4c2f1
HybridSquirrel <robert.squirrel@mailfence.com>
2021-11-10 20:25:44 +0100

    changed colors

--- lua/forest/theme.lua
+++ lua/forest/theme.lua
@@ -181,7 +181,7 @@ end
 
 theme.loadTerminal = function ()
 
-    vim.g.terminal_color_0 = forest.forest1_gui
+    vim.g.terminal_color_0 = forest.darkgray
     vim.g.terminal_color_1 = forest.red
     vim.g.terminal_color_2 = forest.forest14_gui
     vim.g.terminal_color_3 = forest.forest13_gui
@@ -241,13 +241,13 @@ theme.loadTreeSitter = function ()
         TSTypeBuiltin = { fg = forest.blue }, -- For builtin types.
         TSTag = { fg = forest.purble }, -- Tags like html tag names.
         TSTagDelimiter = { fg = forest.purple }, -- Tag delimiter like `<` `>` `/`
-        --TSText = { fg = forest.white }, -- For strings consideforest11_gui text in a markup language.
-        --TSTextReference = { fg = forest.white }, -- FIXME
-        --TSEmphasis = { fg = forest.white }, -- For text to be represented with emphasis.
-        --TSUnderline = { fg = forest.white, bg = forest.none, style = 'underline' }, -- For text to be represented with an underline.
-        --TSTitle = { fg = forest.white, bg = forest.none, style = 'bold' }, -- Text that is part of a title.
-        --TSLiteral = { fg = forest.white }, -- Literal text.
-        --TSURI = { fg = forest.white }, -- Any URI like a link or email.        TSAnnotation =                                                                  { fg = forest.forest11_gui },    -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
+        TSText = { fg = forest.white }, -- For strings consideforest11_gui text in a markup language.
+        TSTextReference = { fg = forest.white }, -- FIXME
+        TSEmphasis = { fg = forest.white }, -- For text to be represented with emphasis.
+        TSUnderline = { fg = forest.white, bg = forest.none, style = 'underline' }, -- For text to be represented with an underline.
+        TSTitle = { fg = forest.white, bg = forest.none, style = 'bold' }, -- Text that is part of a title.
+        TSLiteral = { fg = forest.white }, -- Literal text.
+        TSURI = { fg = forest.white }, -- Any URI like a link or email.        TSAnnotation =                                                                  { fg = forest.forest11_gui },    -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
 
     }
 
